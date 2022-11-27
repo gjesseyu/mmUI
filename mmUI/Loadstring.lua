967c241e4f06d36d3b97e694597537998ca46754
gjesseyu <119155553+gjesseyu@users.noreply.github.com>
2022-11-27 09:43:50 +0100

    pb

--- mmUI/Loadstring.lua
+++ mmUI/Loadstring.lua
@@ -1,1 +1,5 @@
-loadstring(game:HttpGet(("https://raw.githubusercontent.com/gjesseyu/Lua-Projects/main/mmUI/Main.lua"), true))()
+-- Github (gets updated)
+loadstring(game:HttpGet("https://raw.githubusercontent.com/gjesseyu/Lua-Projects/main/mmUI/Main.lua"))();
+
+-- Pastebin (outdated)
+loadstring(game:HttpGet("https://pastebin.com/raw/BhHn1ijh"))();
