227686ccf3b15e5661b553b5d7895189ef6aa763
gjesseyu <119155553+gjesseyu@users.noreply.github.com>
2022-11-28 14:55:15 +0100

    Delete Loadstring.lua

--- mmUI/Loadstring.lua
+++
@@ -1,5 +0,0 @@
--- Github (gets updated)
-loadstring(game:HttpGet("https://raw.githubusercontent.com/gjesseyu/Lua-Projects/main/mmUI/Main.lua"))();
-
--- Pastebin (outdated)
-loadstring(game:HttpGet("https://pastebin.com/raw/BhHn1ijh"))();
