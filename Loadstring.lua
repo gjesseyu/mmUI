9c46955051c042573c4655f5027954d474506c91
gjesseyu <119155553+gjesseyu@users.noreply.github.com>
2022-11-28 14:55:04 +0100

    Create Loadstring.lua

---
+++ Loadstring.lua
@@ -0,0 +1,5 @@
+-- Github (gets updated)
+loadstring(game:HttpGet("https://raw.githubusercontent.com/gjesseyu/Lua-Projects/main/mmUI/Main.lua"))();
+
+-- Pastebin (outdated)
+loadstring(game:HttpGet("https://pastebin.com/raw/BhHn1ijh"))();
